84a8d2fb7155ab09c784a91a6fdb538f1b322a07
Diego <newskyforge2@hotmail.com>
2021-12-11 19:54:16 -0600

    Add todo model

---
+++ src/models/todo/todoModel.js
@@ -0,0 +1,43 @@
+import Todo from './todo.js';
+
+const todoModel = (function () {
+  const todoList = [];
+  let todoId = 0;
+
+  function createTodo(
+    title,
+    project,
+    desc = null,
+    dueDate = null,
+    priority = null,
+    notes = null,
+    complete = false
+  ) {
+    todoId += 1;
+    const todo = Todo(todoId, title, desc, dueDate, priority, notes, complete);
+    todoList.push(todo);
+  }
+
+  function removeTodo(id) {
+    const index = todoList.findIndex((todo) => todo.getId() === id);
+    if (index >= 0) todoList.splice(index, 1);
+    return index;
+  }
+
+  function editTodo(id, data) {
+    const todo = getTodo(id);
+    if (todo) Object.assign(todo, data);
+  }
+
+  function getTodo(id) {
+    return todoList.find((todo) => todo.getId() === id);
+  }
+
+  function getTodosFromIdList(idList) {
+    return todoList.filter((todo) => idList.includes(todo.getId()));
+  }
+
+  return { createTodo, getTodo, removeTodo, editTodo, getTodosFromIdList };
+})();
+
+export default todoModel;
