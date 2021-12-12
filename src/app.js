d3ed6d0135252e44a52f77a623a2f53c20082ea2
Diego <newskyforge2@hotmail.com>
2021-12-12 10:56:35 -0600

    Add controller and views

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+import projectsController from './controllers/projectsController';
+import eventAggregator from './eventAggregator';
+import projectForm from './views/projectForm';
 import sidebar from './views/sidebarView';
 
 const content = document.getElementById('content');
 
 function App(container) {
+  const formProject = projectForm();
+  container.append(formProject);
+
+  eventAggregator.subscribe('showProjectForm', onShowProjectForm);
+
+  function onShowProjectForm() {
+    formProject.classList.remove('hidden');
+  }
+  projectsController.initialize();
   sidebar.initialize(container);
 }
 
