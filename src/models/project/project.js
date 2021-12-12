3bc1b650f5d306a34f2f2d0d8cfcc29e93230eaa
Diego <newskyforge2@hotmail.com>
2021-12-11 19:09:18 -0600

    Add project factory

---
+++ src/models/project/project.js
@@ -0,0 +1,27 @@
+function Project(id, name) {
+  const todoIdList = [];
+
+  function getId() {
+    return id;
+  }
+
+  function deleteTodo(id) {
+    const index = todoIdList.indexOf(id);
+    if (index >= 0) todoIdList.splice(index, 1);
+    return index;
+  }
+
+  function addTodo(id) {
+    if (todoIdList.includes(id)) return null;
+    todoIdList.push(id);
+    return id;
+  }
+
+  function getTodoIdList() {
+    return todoIdList;
+  }
+
+  return { getId, deleteTodo, addTodo, name, getTodoIdList };
+}
+
+export default Project;
