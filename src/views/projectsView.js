1ede5d18632eb908db92d990ddb3e6a598072b0c
Diego <newskyforge2@hotmail.com>
2021-12-12 10:54:40 -0600

    Add projects view of the sidebar

--- src/views/projectsView.js
+++ src/views/projectsView.js
@@ -1,6 +1,10 @@
+import eventAggregator from '../eventAggregator';
+
 const projectsView = (function () {
   let container;
   let projectsNav;
+
+  eventAggregator.subscribe('projectCreated', addProject);
 
   function emptyProjects() {
     while (projectsNav.firstChild) {
@@ -8,11 +12,27 @@ const projectsView = (function () {
     }
   }
 
+  function addProject({ name, projectId }) {
+    const project = document.createElement('button');
+    project.textContent = name;
+    project.dataset.id = projectId;
+    projectsNav.append(project);
+  }
+
+  function onShowProjectForm() {
+    eventAggregator.publish('showProjectForm');
+  }
+
   function render() {
     emptyProjects();
+    const projectsHeader = document.createElement('div');
+    const addBtn = document.createElement('button');
+    addBtn.textContent = '+';
+    addBtn.onclick = onShowProjectForm;
     const title = document.createElement('h2');
     title.textContent = 'Projects';
-    projectsNav.append(title);
+    projectsHeader.append(title, addBtn);
+    projectsNav.append(projectsHeader);
   }
 
   function initialize(viewContainer) {
