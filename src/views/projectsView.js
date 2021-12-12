2303828e52f30e3300c93ef975114d448464bb27
Diego <newskyforge2@hotmail.com>
2021-12-12 11:43:05 -0600

    Add delete project btn and restructre the nav

--- src/views/projectsView.js
+++ src/views/projectsView.js
@@ -3,20 +3,31 @@ import eventAggregator from '../eventAggregator';
 const projectsView = (function () {
   let container;
   let projectsNav;
+  let projectList;
 
   eventAggregator.subscribe('projectCreated', addProject);
 
   function emptyProjects() {
-    while (projectsNav.firstChild) {
-      projectsNav.firstChild.remove();
+    while (projectList.firstChild) {
+      projectList.firstChild.remove();
     }
   }
 
+  function onDelete(e) {
+    const id = e.target.closest('[data-id]')?.dataset.id;
+    eventAggregator.publish('deleteProject', id);
+  }
+
   function addProject({ name, projectId }) {
-    const project = document.createElement('button');
-    project.textContent = name;
+    const project = document.createElement('li');
     project.dataset.id = projectId;
-    projectsNav.append(project);
+    const title = document.createElement('h3');
+    title.textContent = name;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '-';
+    deleteBtn.onclick = onDelete;
+    project.append(title, deleteBtn);
+    projectList.append(project);
   }
 
   function onShowProjectForm() {
@@ -33,7 +44,7 @@ const projectsView = (function () {
     const title = document.createElement('h2');
     title.textContent = 'Projects';
     projectsHeader.append(title, addBtn);
-    projectsNav.append(projectsHeader);
+    projectsNav.append(projectsHeader, projectList);
   }
 
   function initialize(viewContainer) {
@@ -41,6 +52,8 @@ const projectsView = (function () {
     projectsNav = document.createElement('nav');
     projectsNav.id = 'projects-nav';
     projectsNav.classList.add('projects-nav');
+    projectList = document.createElement('ul');
+    projectList.classList.add('project-list');
     container.append(projectsNav);
     render();
   }
