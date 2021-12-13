c353818da9a78a7a9ce6b0b46c5633c933ab4496
Diego <newskyforge2@hotmail.com>
2021-12-13 11:24:38 -0600

    Add todos selected  to the list

--- src/views/todoListView.js
+++ src/views/todoListView.js
@@ -1,15 +1,33 @@
+import eventAggregator from '../eventAggregator';
+import todoItem from './todoItem';
+
 const todoListView = (function () {
   let container;
   let todosContainer;
   let todoList;
+  let projectTitle;
+
+  function onTodosSelected({ name, filteredList }) {
+    filteredList = filteredList.map(
+      ({ getId, title, priority, desc, dueDate, completed }) =>
+        todoItem(getId(), title, priority, desc, dueDate, completed)
+    );
+    todoList.append(...filteredList);
+    projectTitle.textContent = name;
+  }
 
   function initialize(viewContainer) {
     container = viewContainer;
     todosContainer = document.createElement('main');
-    const projectTitle = document.createElement('h2');
+    projectTitle = document.createElement('h2');
     todoList = document.createElement('ul');
-    todosContainer.append(projectTitle, todoList);
+    const addTodoBtn = document.createElement('button');
+    addTodoBtn.type = 'button';
+    addTodoBtn.textContent = 'Add Todo';
+    todosContainer.append(projectTitle, todoList, addTodoBtn);
     container.append(todosContainer);
+
+    eventAggregator.subscribe('todosSelected', onTodosSelected);
   }
 
   return { initialize };
